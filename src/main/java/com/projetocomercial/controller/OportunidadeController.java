a4175198b638ae6d79f59f2444e7cc4eb50ac0fe
devfabio <fabiohgoulart9@gmail.com>
2019-05-10 14:32:25 -0300

    Implementação do metodo buscar na controller oportunidades (já tratando os códigos de retorno 200,404).

--- src/main/java/com/projetocomercial/controller/OportunidadeController.java
+++ src/main/java/com/projetocomercial/controller/OportunidadeController.java
@@ -3,11 +3,14 @@ package com.projetocomercial.controller;
 import com.projetocomercial.model.Oportunidade;
 import com.projetocomercial.repository.OportunidadeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/oportunidades")
@@ -21,4 +24,14 @@ public class OportunidadeController {
         return oportunidades.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Oportunidade> buscar(@PathVariable Long id){
+
+        Optional<Oportunidade> oportunidade = oportunidades.findById(id);
+
+        if (!oportunidade.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(oportunidade.get());
+    }
 }
