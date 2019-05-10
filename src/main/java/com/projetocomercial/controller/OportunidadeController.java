1ffa24ca6e4bcd80fbbe6938ddb6b631f274f31d
devfabio <fabiohgoulart9@gmail.com>
2019-05-10 14:26:59 -0300

    Implementação do metodo listar na controller oportunidades (OportunidadeController.java).

--- src/main/java/com/projetocomercial/controller/OportunidadeController.java
+++ src/main/java/com/projetocomercial/controller/OportunidadeController.java
@@ -1,4 +1,24 @@
 package com.projetocomercial.controller;
 
+import com.projetocomercial.model.Oportunidade;
+import com.projetocomercial.repository.OportunidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/oportunidades")
 public class OportunidadeController {
+
+    @Autowired
+    private OportunidadeRepository oportunidades;
+
+    @GetMapping
+    public List<Oportunidade> listar(){
+        return oportunidades.findAll();
+    }
+
 }
