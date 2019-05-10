77a12123f46be8a6105b67af162d5ed9fbc9b2ec
devfabio <fabiohgoulart9@gmail.com>
2019-05-10 09:34:46 -0300

    Initial commit

---
+++ src/main/java/com/projetocomercial/ProjetoSpringComercialApplication.java
@@ -0,0 +1,13 @@
+package com.projetocomercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringComercialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoSpringComercialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projetocomercial/ProjetoSpringComercialApplicationTests.java
@@ -0,0 +1,16 @@
+package com.projetocomercial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoSpringComercialApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
