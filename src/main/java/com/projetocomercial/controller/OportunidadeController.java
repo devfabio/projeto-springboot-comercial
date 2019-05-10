8dea688a39c0600e74aa6a2e4f65e0e4b27132ff
devfabio <fabiohgoulart9@gmail.com>
2019-05-10 14:48:17 -0300

    Implementação do metodo adicionar junto a controller oportunidades.

--- src/main/java/com/projetocomercial/controller/OportunidadeController.java
+++ src/main/java/com/projetocomercial/controller/OportunidadeController.java
@@ -3,11 +3,9 @@ package com.projetocomercial.controller;
 import com.projetocomercial.model.Oportunidade;
 import com.projetocomercial.repository.OportunidadeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,5 +31,11 @@ public class OportunidadeController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(oportunidade.get());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Oportunidade adicionar(@RequestBody Oportunidade opo){
+        return oportunidades.save(opo);
     }
 }
