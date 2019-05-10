113bbccb95e09677fe68bc66d996bf320960fc93
devfabio <fabiohgoulart9@gmail.com>
2019-05-10 10:34:58 -0300

    Estruturando o corpo da API

---
+++ src/main/java/com/projetocomercial/controller/OportunidadeController.java
@@ -0,0 +1,4 @@
+package com.projetocomercial.controller;
+
+public class OportunidadeController {
+}
---
+++ src/main/java/com/projetocomercial/repository/OportunidadeRepository.java
@@ -0,0 +1,8 @@
+package com.projetocomercial.repository;
+
+import com.projetocomercial.model.Oportunidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OportunidadeRepository extends JpaRepository <Oportunidade, Long> {
+
+}
