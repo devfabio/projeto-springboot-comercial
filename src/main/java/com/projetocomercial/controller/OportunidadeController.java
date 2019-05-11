39592252a48551195a30298090adbb8ff999f1a2
devfabio <fabiohgoulart9@gmail.com>
2019-05-11 18:39:31 -0300

    Novas implementações (filtros de busca, metodos atualizar e deletar).

--- src/main/java/com/projetocomercial/controller/OportunidadeController.java
+++ src/main/java/com/projetocomercial/controller/OportunidadeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -37,6 +38,35 @@ public class OportunidadeController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Oportunidade adicionar(@Valid @RequestBody Oportunidade opo){
+
+        Optional<Oportunidade> opoExistente = oportunidades.findByDescricaoAndNomeProspecto(opo.getDescricao(),opo.getNomeProspecto());
+
+        if(opoExistente.isPresent()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Já Existe uma oportunidade para este prospecto com a mesma descrição!!!");
+        }
         return oportunidades.save(opo);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletar(@PathVariable Long id){
+
+        Oportunidade opo = oportunidades.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,"Deu zebra!!!"));
+        oportunidades.delete(opo);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Oportunidade> atualizar(@PathVariable Long id,@Valid @RequestBody Oportunidade opo){
+
+        Oportunidade oportunidade = oportunidades.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,"Deu Errado!!!"));
+
+        oportunidade.setNomeProspecto(opo.getNomeProspecto());
+        oportunidade.setDescricao(opo.getDescricao());
+        oportunidade.setValor(opo.getValor());
+
+        Oportunidade oportu = oportunidades.save(oportunidade);
+
+        return ResponseEntity.ok(oportu);
     }
 }
